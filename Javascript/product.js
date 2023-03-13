7137efcf29b9cb5caa7c836367d4c9eeb5804fa6
Carolin <carolinanime12@gmail.com>
2023-03-13 18:52:09 -0400

    Terminando

--- Javascript/product.js
+++ Javascript/product.js
@@ -1,2 +1,17 @@
+// Obtener referencias a los elementos HTML
+const agregarBtn = document.getElementById("agregar-btn");
+const bagCount = document.getElementById("bag-count");
 
+// Obtener el valor actual de la cantidad de ropa en la cartera desde localStorage
+let cantidad = localStorage.getItem("cantidad") || 0;
+
+// Actualizar el texto en el elemento HTML con la cantidad actual
+bagCount.textContent = cantidad;
+
+// Agregar un manejador de eventos al botón para actualizar la cantidad y almacenarla en localStorage
+agregarBtn.addEventListener("click", function() {
+  cantidad++;
+  localStorage.setItem("cantidad", cantidad);
+  bagCount.textContent = cantidad;
+});
 
